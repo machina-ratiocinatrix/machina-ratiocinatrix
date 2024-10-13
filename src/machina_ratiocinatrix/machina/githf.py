0943813496cb18bd02fa8f322589a5504886fbfb
Alex Fedotov <alxfed@gmail.com>
2024-10-13 09:05:35 -0500

    Correction

--- src/machina_ratiocinatrix/machina/githf.py
+++ src/machina_ratiocinatrix/machina/githf.py
@@ -23,9 +23,29 @@ def connectto_repo(organization=None,
                    repository_name=None,
                    private=False):
     """
-    Establish connection with a repository.
-    Return 'None' if connection fails.
+    Establish a connection with a GitHub repository.
+
+    Args:
+        organization (str, optional): The name of the organization. If not provided,
+            the repository is assumed to be owned by the authenticated user.
+        repository_name (str): The name of the repository.
+        private (bool, optional): Whether the repository is private. Defaults to False.
+
+    Returns:
+        github.Repository.Repository: The GitHub repository object if the connection is successful.
+        None: If the connection fails.
+
+    Raises:
+        github.UnknownObjectException: If the repository does not exist.
+
+    Note:
+        The function requires the following environment variables to be set:
+        - GITHUB_TOKEN: The personal access token for authentication.
+        - GITHUB_NAME: The name of the authenticated user.
+        - GITHUB_EMAIL: The email of the authenticated user.
+
     """
+
     gh = Github(github_token, verify=False)
     if organization:
         org = gh.get_organization(organization)
@@ -47,9 +67,22 @@ def connectto_repo(organization=None,
 
 def read_file(repository,
               file_path):
-    """ Read a file in a repository
-        file_path: path to the file formatted as
-        'directory_in_repo/subdirectory/file.ext'
+    """
+    Read the contents of a file in a GitHub repository.
+
+    Args:
+        repository (github.Repository.Repository): The GitHub repository object.
+        file_path (str): The path to the file in the repository, formatted as
+            'directory_in_repo/subdirectory/file.ext'.
+
+    Returns:
+        str: The contents of the file as a string. If the file does not exist, an empty string is returned.
+
+    Raises:
+        github.UnknownObjectException: If the file does not exist in the repository.
+
+    Note:
+        This function assumes that the repository object has already been authenticated and connected.
     """
     try:
         # Get the file if it exists
